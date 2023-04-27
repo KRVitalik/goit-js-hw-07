84ab3a0637651e849e90510d9e8faad109982e94
KRVitalik <karmelitavitalik@gmail.com>
2023-04-27 21:02:38 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+galleryItems.forEach(item => createCatalog(item));
+
+function createCatalog(item) {
+const listElement = document.createElement('li')
+listElement.classList.add('gallery__item')
+
+const refElement = document.createElement('a')
+refElement.classList.add('gallery__link')
+refElement.setAttribute('href', item.original)
+
+const imgElement = document.createElement('img')
+imgElement.classList.add('gallery__image')
+imgElement.setAttribute('src', `${item.preview}`)
+imgElement.setAttribute('data-source', `${item.original}`)
+imgElement.setAttribute('alt', `${item.description}`)
+
+listElement.appendChild(refElement)
+    refElement.appendChild(imgElement) 
+    
+    console.log(listElement);
+}
+
+
