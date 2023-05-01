2828da7aca9ee484a8fbb8a4c2735fca7db36fcc
KRVitalik <karmelitavitalik@gmail.com>
2023-05-01 15:06:02 +0300

    lightbox-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imageContainer = document.querySelector('.gallery');
+const allGalleryImages = createGalleryImages(galleryItems);
+
+imageContainer.insertAdjacentHTML('beforeend', allGalleryImages)
+
+function createGalleryImages(images) {
+    return images
+    .map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </li>`
+    }).join('')
+};
+
+imageContainer.addEventListener('click', (onImageClick))
+
+function onImageClick(e) {
+    e.preventDefault();
+
+    if (e.target.classList.value !== 'gallery__image') {
+        return
+    }
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+})
